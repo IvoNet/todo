34324ed9e226786825db4c7ce2a8e3dc8681f500
ivonet <ivonet@gmail.com>
2014-11-19 00:59:02 +0100

    step with hello world frond end but full back-end

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -14,50 +14,12 @@ app.config([
               }
            ]);
 
-app.factory('restService', function ($http) {
-   return {
-      query: function () {
-         return $http({
-                         method: 'GET',
-                         url   : 'resources/todos'
-                      });
-      },
-      post : function (url, object) {
-         return $http({
-                         method : 'POST',
-                         url    : url,
-                         headers: {'Content-Type': 'application/json'},
-                         data   : object
-                      });
-      }
-   };
-});
 
 app.controller('HomeController', [
    '$scope',
-   '$filter',
-   'restService',
-   function ($scope, $filter, restService) {
+   function ($scope) {
       $scope.debug = true;
-      $scope.title = "Be calm... Todo";
-
-      restService.query().success(function (data) {
-         $scope.data = data;
-      });
-
-      $scope.totaalTodos = function () {
-         if ($scope.data !== undefined) {
-            return $scope.data.length;
-         }
-         return 0;
-      };
-
-
-
-
-      $scope.$watch('data', function () {
-         restService.post('resources/todos', $scope.data)
-      }, true);
+      $scope.title = "Hello World";
 
    }
 ]);
